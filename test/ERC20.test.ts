e6e73ffad605276278cd6a4cf7faadd019e89ec4
Lucas Oliveira <olivmath@protonmail.com>
2022-10-27 22:43:43 -0300

    update: test

--- test/ERC20.test.ts
+++ test/ERC20.test.ts
@@ -3,7 +3,7 @@ import { HelloWorld__factory, HelloWorld } from "../typechain-types"
 import { ethers as hardhat } from "hardhat"
 import { expect } from "chai"
 
-describe("Lua Token", () => {
+describe("Hello Contract", () => {
     let HelloWorldContract: HelloWorld
     let owner: SignerWithAddress
     let addrs: SignerWithAddress[]
