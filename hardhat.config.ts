b79d4046f8c32aad672be3a90f04d8cf95abfd1d
Lucas Oliveira <50037567+olivmath@users.noreply.github.com>
2022-11-10 20:40:03 -0300

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config"
+import { HardhatUserConfig } from "hardhat/types"
 import "@nomicfoundation/hardhat-toolbox"
+import { config } from "dotenv"
 
-const config: HardhatUserConfig = {
-    solidity: "0.8.17"
+config()
+// const { API_URL, PRVK } = process.env
+
+module.exports = {
+    solidity: "0.8.17",
+    networks: {
+        // defaultNetwork: "matic",
+        // matic: {
+        //     url: API_URL,
+        //     accounts: [`0x${PRVK}`]
+        // }
+        hardhat: {
+            mining: {
+                auto: true,
+                interval: 5000
+            }
+        }
+    }
 }
-
-export default config
