02911436f80fb1e89ba86335c71ee25f5238452f
Lucas Oliveira <50037567+olivmath@users.noreply.github.com>
2022-11-10 21:20:33 -0300

    Update HelloWorld.test.ts

--- test/HelloWorld.test.ts
+++ test/HelloWorld.test.ts
@@ -1,26 +1,39 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { HelloWorld__factory, HelloWorld } from "../typechain-types"
 import { ethers as hardhat } from "hardhat"
-import { expect } from "chai"
 
 describe("Hello World Contract", () => {
-    let HelloWorldContract: HelloWorld
+    /**
+     * @DeclareWallet
+     * - Owner
+     * - Others
+     */
     let owner: SignerWithAddress
-    let addrs: SignerWithAddress[]
+    let others: SignerWithAddress[]
 
-    beforeEach(async () => {
-        ;[owner, ...addrs] = await hardhat.getSigners()
+    /**
+     * @DeclareSmartContracts
+     * - HelloWorld
+     */
+    let hw: HelloWorld
 
-        const HelloWorldContractFactory = (await hardhat.getContractFactory(
-            "HelloWorld",
-            owner
-        )) as HelloWorld__factory
-        HelloWorldContract = await HelloWorldContractFactory.deploy()
+    describe("Wallets", async () => {
+        it("Create Wallets", async () => {
+            ;[owner, ...others] = await hardhat.getSigners()
+        })
+    })
+
+    describe("Deploy", async () => {
+        it("Deploy HelloWorld Contract", async () => {
+            const HelloWorld: HelloWorld__factory =
+                await hardhat.getContractFactory("HelloWorld")
+            hw = await HelloWorld.deploy()
+        })
     })
 
     describe("Deployment", () => {
         it("Should return `Hello World` from msg ", async () => {
-            const msg = await HelloWorldContract.connect(owner).msg()
+            const msg = await hw.connect(owner).msg()
             expect(msg).to.equal("Hello World")
         })
     })
